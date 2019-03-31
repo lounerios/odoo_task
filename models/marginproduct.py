bd2f0247a0fe38a325ccc6d13904f04416face01
root <root@vagrant.vm>
2019-03-31 11:32:52 +0000

    FInish task #2

--- models/marginproduct.py
+++ models/marginproduct.py
@@ -2,6 +2,9 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo.addons import decimal_precision as dp
 from odoo import api, fields, models
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class SalesMarginProduct(models.Model):
     _inherit = 'product.template'
@@ -25,3 +28,18 @@ class SalesMarginOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     sales_margin = fields.Float(string='Sales Margin',digits=(1,2), default='1.00')
+    cost = fields.Float(string='Cost', related='product_id.product_tmpl_id.standard_price', readonly=True)
+
+    @api.onchange('product_id')
+    def product_is_changed(self):
+        if(self.product_id):
+            _logger.debug('Product is changed')
+            product_template = self.env['product.template'].browse([self.product_id.product_tmpl_id.id])
+            self.sales_margin = product_template.sales_margin
+
+    @api.onchange('price_unit')
+    def price_unit_is_changed(self):
+        _logger.debug('Price unit is changed')
+        if(self.price_unit > 0):
+            product_template = self.env['product.template'].browse([self.product_id.product_tmpl_id.id])
+            self.sales_margin = product_template.standard_price / self.price_unit
